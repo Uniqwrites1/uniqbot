7aeb288cfdba4c6820ae35fe87183476b8f77469
Uniqwrites1 <uniqchild1@gmail.com>
2025-08-27 12:29:04 +0100

    Add home page to fix 404 error on root URL

--- uniqwrites/urls.py
+++ uniqwrites/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Uniqwrites WhatsApp Bot is running!")
 
 urlpatterns = [
+    path('', home, name='home'),
     path('admin/', admin.site.urls),
     path('webhook/', include('whatsapp_bot.urls')),
 ]
